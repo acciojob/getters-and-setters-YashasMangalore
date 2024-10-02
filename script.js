fd6bf31bc7d062aed203843f56dc9d76d3194bbb
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-10-02 12:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-//complete this code
 class Person {
-	constructor(name,age)
-	{
-		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		return this.name;
-	}
-	
-	get age(){
-		this.age=age;
-	}
-	set age(age){
-		this.age=age;
-	}
+    constructor(name, age) {
+        this._name = name; // Store name in a private variable
+        this._age = age;   // Store age in a private variable
+    }
+
+    get name() {
+        return this._name; // Getter for name
+    }
+
+    set age(value) {
+        this._age = value; // Setter for age
+    }
+
+    get age() {
+        return this._age; // Getter for age if you need to access it
+    }
 }
 
 class Student extends Person {
-	study()
-	{
-		console.log(`${this.name} is studying`);
-	}
+    study() {
+        console.log(`${this.name} is studying`); // Log studying
+    }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+    teach() {
+        console.log(`${this.name} is teaching`); // Log teaching
+    }
 }
 
 // Do not change the code below this line
